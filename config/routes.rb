bc021f5d523651a018dee2eb20e6bcd2debe4b6d
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-10-18 12:44:52 +0900

    `StaticPages`コントローラーを生成した
    Home などのページや Logout などのリンクがないと、`Devise`gem を使った
    開発が進みにくいことに気付いた。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/help',         to: 'static_pages#help'
+  get '/about',        to: 'static_pages#about'
+  get '/contact_us',   to: 'static_pages#contact_us'
   devise_for :users
-  root "users#index"
+  root "static_pages#about"
 end
