c5976262e335ed5adf69f570ada44d4be1152664
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-10-20 20:08:21 +0900

    `Media`テーブルをマイグレートする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_082534) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_20_093710) do
+  create_table "media", force: :cascade do |t|
+    t.string "name"
+    t.string "furigana"
+    t.string "website_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["furigana"], name: "index_media_on_furigana"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
