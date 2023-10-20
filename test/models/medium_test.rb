efe102905ae76c6c7617eccccf7303aa60bedc53
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-10-20 20:11:32 +0900

    `Medium`モデルのテストを書いた

--- test/models/medium_test.rb
+++ test/models/medium_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class MediumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @medium = media(:tbs)
+  end
+
+  test "有効である" do
+    assert @medium.valid?
+  end
+
+  test "名前は空白であってはならない" do
+    @medium.name = ''
+    assert_not @medium.valid?
+  end
+
+  test "振り仮名は空白であってはならない" do
+    @medium.furigana = ''
+    assert_not @medium.valid?
+  end
+
+  test "ウェブサイトURLは空白であっても良い" do
+    @medium.website_url = ''
+    assert @medium.valid?
+  end
+
+  test "ウェブサイトURLが空白でなければ、実在しなければならない" do
+    @medium.website_url = 'https://example.false'
+    assert_not @medium.valid?
+  end
+
+  test "ウェブサイトURLのスキームはHTTPでなければならない" do
+    @medium.website_url.sub! /^http/, 'ftp'
+    assert_not @medium.valid?
+  end
 end
