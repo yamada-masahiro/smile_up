aa0004b167867daf8770f86b05528a72f1243de9
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-10-28 03:07:04 +0900

    独立とメディア ID のバリデーションを書いた
    これにより、次のテストが通った。
    - 独立であれば、メディア ID は nil でなければならない
    
    合わせて、名前も正規化するようにした。

--- app/models/reporter.rb
+++ app/models/reporter.rb
@@ -1,5 +1,5 @@
 class Reporter < ApplicationRecord
-  belongs_to :medium
+  belongs_to :medium, optional: true
   enum :desirability,
     { highly:      2,
       somewhat:    1,
@@ -9,9 +9,20 @@ class Reporter < ApplicationRecord
     },
     default: :neutral
   # String#my_prettify!: config/initializers/string_extensions.rb
-  before_validation -> { self.furigana.my_prettify! }, on: [:create, :update]
+  before_validation -> { self.name.my_prettify!; self.furigana.my_prettify! }, on: [:create, :update]
   validates :name,        presence: true
   validates :furigana,    presence: true,
                           format: { with: /\A[\p{Hiragana}ー・]+( [\p{Hiragana}ー・]+)*\z/,
                                     message: 'は、ひらがなで記してください' }
+  validate :validate_medium_id_based_on_independent
+
+private
+
+  def validate_medium_id_based_on_independent
+    if independent? && medium_id.present?
+      errors.add(:medium_id, "独立である場合は、メディアは指定できません")
+    elsif !independent? && (medium_id.nil? || !Medium.exists?(medium_id))
+      errors.add(:medium_id, "独立でない場合は、メディアを指定してください")
+    end
+  end
 end
