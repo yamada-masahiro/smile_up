c62a4dea52ae37f7fd065b84a55e0c9452139b16
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-10-27 23:26:17 +0900

    簡単なバリデーションを通した
    - 名前は空白であってはならない
    - 振り仮名は空白であってはならない
    - 振り仮名はひらがなであること

--- app/models/reporter.rb
+++ app/models/reporter.rb
@@ -1,3 +1,19 @@
 class Reporter < ApplicationRecord
   belongs_to :medium
+  enum :desirability,
+    { highly:      2,
+      somewhat:    1,
+      neutral:     0,
+      not_very:   -1,
+      not_at_all: -2
+    },
+    default: :neutral
+  # before_save { self.furigana.gsub!(/\s+/, '') }
+  validates :name,        presence: true
+  validates :furigana,    presence: true,
+                          format: { with: /\A[\p{Hiragana}ー・]+( [\p{Hiragana}ー・]+)*\z/,
+                                    message: 'は、ひらがなで記してください' }
+  validates :independent, presence: true,
+                          inclusion: { in: [false, true] }
+  validates :medium_id,   presence: true, unless: :independent?
 end
