41350d6f3f118ce78388fe6cccf47516f31bbbb5
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-10-29 04:51:40 +0900

    SMU-2 属性の名前を訂正した
    `rails g scaffold`の後に属性`degree_of_welcome`を`desirability`に変更
    したが、テストやビューに古いものが残っていた。テストに先立って、これら
    を訂正した。
    その結果、テストが全てパスした。しかし、これから、本来の仕様をテストに
    反映する。

--- test/controllers/reporters_controller_test.rb
+++ test/controllers/reporters_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ReportersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @reporter = reporters(:one)
+    @reporter = reporters(:homma)
   end
 
   test "should get index" do
@@ -17,7 +17,7 @@ class ReportersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create reporter" do
     assert_difference("Reporter.count") do
-      post reporters_url, params: { reporter: { degree_of_welcome: @reporter.degree_of_welcome, furigana: @reporter.furigana, medium_id: @reporter.medium_id, name: @reporter.name } }
+      post reporters_url, params: { reporter: { desirability: @reporter.desirability, furigana: @reporter.furigana, medium_id: @reporter.medium_id, name: @reporter.name } }
     end
 
     assert_redirected_to reporter_url(Reporter.last)
@@ -34,7 +34,7 @@ class ReportersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update reporter" do
-    patch reporter_url(@reporter), params: { reporter: { degree_of_welcome: @reporter.degree_of_welcome, furigana: @reporter.furigana, medium_id: @reporter.medium_id, name: @reporter.name } }
+    patch reporter_url(@reporter), params: { reporter: { desirability: @reporter.desirability, furigana: @reporter.furigana, medium_id: @reporter.medium_id, name: @reporter.name } }
     assert_redirected_to reporter_url(@reporter)
   end
 
