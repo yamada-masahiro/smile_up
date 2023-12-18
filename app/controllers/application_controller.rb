78f090dfa9874ff3ad8a38a83375cfa19a4f4033
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-12-18 19:55:57 +0900

    `logged_in?`メソッド等を定義した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?, :current_user
+
+private
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find session[:user_id]
+  end
 end
